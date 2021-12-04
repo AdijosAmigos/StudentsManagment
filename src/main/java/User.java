5bc93a4277e532f6180df5bf37356f1cef7b7dd6
AdijosAmigos <adrian.gurda10@gmail.com>
2021-12-04 14:09:12 +0100

    class User and enum Type

--- src/main/java/User.java
+++ src/main/java/User.java
@@ -1,3 +1,25 @@
 public class User {
 
+    private final Integer id;
+    private final String login;
+    private final String password;
+    private final String name;
+    private final String surname;
+    private final String PID;
+    private final UserType type;
+
+
+    public User(Integer id, String login, String password, String name, String surname, String PID, UserType type) {
+        this.id = id;
+        this.login = login;
+        this.password = password;
+        this.name = name;
+        this.surname = surname;
+        this.PID = PID;
+        this.type = type;
+    }
+
+    public UserType getType() {
+        return type;
+    }
 }
