bc335fb37494430b50a6e8d45e077d40160705ce
AdijosAmigos <adrian.gurda10@gmail.com>
2021-12-13 21:10:41 +0100

    testy do CourseService

--- src/test/java/CourseServiceTest.java
+++ src/test/java/CourseServiceTest.java
@@ -11,16 +11,8 @@ class CourseServiceTest {
     @Test
     void should_sign_up_student_to_course() {
         //given
-        ArrayList<User> studensList = new ArrayList<>();
-        HashMap<Integer, Course> idToCourse = new HashMap<>();
-        User user = new User(1, "ad", "12345", "adrian", "nowak", "123456789", UserType.STUDENT);
-        Course course = new Course(1, "math", studensList);
-        CourseRepository courseInMemoryReposiotory = new CourseInMemoryRepository(idToCourse);
-        course.addStudent(user);
         //when
-        Course result = courseInMemoryReposiotory.save(course);
         //then
-        Assertions.assertThat(result).isEqualTo(course);
     }
 
 }
