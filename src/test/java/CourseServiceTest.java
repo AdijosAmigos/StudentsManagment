6b26cce34807bd3165b6807371289d985c4591db
AdijosAmigos <adrian.gurda10@gmail.com>
2021-12-19 09:57:18 +0100

    test signUpStudentToCourse do CourseService

--- src/test/java/CourseServiceTest.java
+++ src/test/java/CourseServiceTest.java
@@ -11,8 +11,19 @@ class CourseServiceTest {
     @Test
     void should_sign_up_student_to_course() {
         //given
+        Course course = new Course(1, "math");
+        User user = new User(1, "adam", "12345", "adam", "nowak", "123456789", UserType.STUDENT);
+        HashMap<Integer, Course> map = new HashMap<Integer, Course>();
+        CourseRepository courseInMemoryRepository = new CourseInMemoryRepository(map);
+        HashMap<Integer, User> map1 = new HashMap<Integer, User>();
+        UserRepository userInMemoryRepository = new UserInMemoryRepository(map1);
+        courseInMemoryRepository.save(course);
+        userInMemoryRepository.save(user);
+        CourseService courseService = new CourseService(courseInMemoryRepository, userInMemoryRepository);
         //when
+        courseService.signUpStudentToCourse(user.getId(), course.getId());
         //then
+
     }
 
 }
